44b73108784d8b8d8fdcfe9cbd4e7b351f50da63
pushorigin <info@pushorigin.ru>
2017-02-17 03:13:58 +0300

    веб-отчет

--- README.md
+++ README.md
@@ -1,19 +1,48 @@
-Описание
-========
+# Описание
 
 Работа с helpdesk.
 
 # Установка
 
+Mongodb:
+
 ```
 db.tasks.createIndex({Id: 1}, {unique: true});
 ```
 
-# gettasks.js
+Bash:
+
+```
+mkdir etc && cp etc.dist/config.json etc
+vim etc/config.json
+```
+
+Повесить на крон:
+
+```
+node gettasks.js
+node telegram.js
+node taskexpenses.js
+```
+
+Запустисть веб-сервер:
+
+```
+node report.js
+```
+
+Browser:
+```
+http://localhost:8080/calendar.html?ExecutorId=14195
+```
+
+# Скрипты
+
+## gettasks.js
 
 Получение заявок. Первый раз следует указать в фильтре "&pagesize=500", затем настроить расписание и уменьшить до (50), так как будут регистрироваться только последние.
 
-# taskexpenses.js
+## taskexpenses.js
 
 Получение трудозатрат. Требования:
 
@@ -21,26 +50,30 @@ db.tasks.createIndex({Id: 1}, {unique: true});
 * оперативное получение
 * не грузить HTTP
 
-поэтому также загребается по "расписанию". Дальше видно будет, стоит ли переделывать на live-версию. Увы, одним запросом к API Intraservice все не вытащить.
+Загребается по "расписанию" (в пользу оперативности). Дальше видно будет, стоит ли переделывать на live-версию. Увы, одним запросом к API Intraservice все не вытащить.
 
-# telegram.js
+## telegram.js
 
-Отправка уведомлений в Telegram
+Отправка уведомлений о новых задачах в Telegram
 
-# report.js
+## report.js
 
-Отчет о задачах
-
-# Разная всячина
-
-* http://helpdesk/api/task?ExecutorIds=11184&sort=Created%20desk
-* http://helpdesk/api/task?ExecutorIds=11184&sort=Created%20desk&Id=116080
-* http://helpdesk/api/tasklifetime?taskid=116080
-
+Отчет о задачах в формате JSON
 
 # Полезные поля для фильтрации
 
-* ChangedMoreThan Дата последнего изменения заявок больше или равна указанной
-* Closed Дата закрытия заявок равна указанному дню (время не учитывается, т.е. с 00:00 до 23:59 указанного дня)
-* Hours Трудозатраты в часах
-* Deadline
+* ChangedMoreThan - Дата последнего изменения заявок больше или равна указанной
+* Closed - Дата закрытия заявок равна указанному дню (время не учитывается, т.е. с 00:00 до 23:59 указанного дня)
+* Hours - Трудозатраты в часах
+* Deadline - Дедлайн
+
+# Тестовые ID
+
+* 11184
+* 11273
+* 14195
+
+# TODO
+
+* Получить статусы http://helpdesk/api/taskstatus
+* Получить пользователей http://helpdesk/api/user
--- public/asset/js/report.js
+++ public/asset/js/report.js
@@ -1,19 +1,121 @@
 (function ($) {
+
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+  };
+
+  var ExecutorId = getUrlParameter('ExecutorId');
+
   $(document).ready(function(){
     $.ajax({
-      url: "http://localhost:8080/report.json",
+      url: "report.json",
       method: 'POST',
       data: {
-        ExecutorId: "11184"
+        ExecutorId: ExecutorId
       },
       cache: false,
       dataType: 'json',
       error: function(){
-        // alert('Упс, данные для отчета не получены');
-        console.log('Упс, данные для отчета не получены');
+        $('#calendar').html('Упс, данные для отчета не получены');
       },
       success: function(resp) {
-        console.log(resp);
+        var events = [];
+        // var $tasksList = $('.js-tasks-list');
+        var $calendarGrid = $('.fc-day-grid');
+
+        for(var i in resp) {
+          var task = resp[i];
+          // $tasksList.append("<div class='fc-event' style='margin-bottom:5px; padding: 5px; font-size:14px;'>[" + task.Id + '] ' +task.Name + "</div>");
+
+          if(typeof task.Expenses !== 'undefined' && task.Expenses.length) {
+
+            for(var i2 in task.Expenses) {
+              var expense = task.Expenses[i2];
+              if(expense.UserId != ExecutorId) continue;
+
+              var expHours = Math.round(expense.Minutes / 6) / 10;
+
+              events.push(
+                {
+                  title: task.Name,
+                  start: expense.Date.substr(0,10),
+                  className: 'task-status--' + task.StatusId,
+                  data: {
+                    url: 'http://helpdesk/Task/View/' + task.Id,
+                    expHours: expHours
+                  }
+                }
+              );
+
+            }
+          }
+
+        }
+
+        // $('#calendar').fullCalendar( 'renderEvent', {title: 'test', start: '2017-02-01'} );
+
+        /*
+        $('#external-events .js-tasks-list .fc-event').each(function() {
+          // store data so the calendar knows to render an event upon drop
+          $(this).data('event', {
+            title: $.trim($(this).text()), // use the element's text as the event title
+            stick: true // maintain when user navigates (see docs on the renderEvent method)
+          });
+
+          // make the event draggable using jQuery UI
+
+          $(this).draggable({
+            zIndex: 999,
+            revert: true,      // will cause the event to go back to its
+            revertDuration: 0  //  original position after the drag
+          });
+        });*/
+
+
+
+        $('#calendar').fullCalendar({
+          header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+          },
+          events: events,
+          firstDay: 1,
+          hiddenDays: [ 0, 6 ],
+          dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота' ],
+          dayNamesShort: ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'суб' ],
+          monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+          locale: 'ru',
+          editable: true,
+          droppable: true, // this allows things to be dropped onto the calendar
+          drop: function(date, jsEvent, ui ) {
+            // is the "remove after drop" checkbox checked?
+            if ($('#drop-remove').is(':checked')) {
+              // if so, remove the element from the "Draggable Events" list
+              $(this).remove();
+            }
+          },
+          eventClick: function(calEvent, jsEvent, view) {
+            // console.log(calEvent);
+          },
+          eventRender: function(event, element) {
+            element.html('<span class="task-hours">' + event.data.expHours +'</span>' + event.title);
+          }
+        });
+
+
+
       }
     })
   });
